2efdc588ef0669e579dbad3879899ed21cf2fd9a
Theresa Bates <theresabates@Theresas-MacBook-Air.local>
2017-04-06 23:21:33 -0400

    Updating files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double cheese burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegi burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger', false);
